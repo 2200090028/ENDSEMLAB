81ffff4d76e5787dfa9e65c4b318dc368fa175ff
Nallani Sravya sri <153180095+2200090028@users.noreply.github.com>
2024-12-08 01:17:59 +0530

    Update StudentService.java

--- SpringBootRestAPIJPAProject/src/main/java/com/klef/jfsd/springboot/service/StudentService.java
+++ SpringBootRestAPIJPAProject/src/main/java/com/klef/jfsd/springboot/service/StudentService.java
@@ -8,8 +8,6 @@ import com.klef.jfsd.springboot.model.Student;
 public interface StudentService 
 {
 	public String addstudent(Student s);
-	public String updatestudent(Student s);
-	public String deletestudent(int sid);
 	public Student viewstudentbyid(int sid);
 	public List<Student> viewallstudents();
 }
